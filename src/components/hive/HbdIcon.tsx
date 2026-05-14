63be2f23039baa27b188388ddba5363cceb138d6
Ing. Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2026-05-14 15:47:01 -0400

    feat: add HiveIcon

---
+++ src/components/hive/HbdIcon.tsx
@@ -0,0 +1,6 @@
+import type { HiveIconProps } from '@/components/hive/HiveIcon'
+import HiveIcon from '@/components/hive/HiveIcon'
+
+export default function HbdIcon(props: HiveIconProps) {
+  return <HiveIcon color="green.fg" colorPalette="green" {...props} />
+}
